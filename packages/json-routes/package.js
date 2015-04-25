f75cd52a6be88ace710f4ac62ae731cbf47b5b85
Sashko Stubailo <sashko@stubailo.com>
2015-04-24 18:37:24 -0700

    Update package.js

--- packages/json-routes/package.js
+++ packages/json-routes/package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'simple:json-routes',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'The simplest way to define server-side routes that return JSON',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/stubailo/meteor-rest',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'

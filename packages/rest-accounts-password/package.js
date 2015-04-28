f290ea15230bb352a73fd096b188ccada95e467f
Sashko Stubailo <sashko@stubailo.com>
2015-04-27 23:18:30 -0700

    Publish beta

--- packages/rest-accounts-password/package.js
+++ packages/rest-accounts-password/package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'simple:rest-accounts-password',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Get a login token to use with simple:rest',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/stubailo/meteor-rest',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'

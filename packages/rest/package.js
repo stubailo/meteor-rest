682ec90b4fb9688c52ec7b75d4dddb11913ef930
Sashko Stubailo <sashko@stubailo.com>
2015-04-28 11:21:59 -0700

    Update github URL

--- packages/rest/package.js
+++ packages/rest/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'The easiest way to add a REST API to your Meteor app',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/stubailo/meteor-rest',
+  git: 'https://github.com/stubailo/meteor-rest/blob/master/packages/rest/',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'

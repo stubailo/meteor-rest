7a2cd2664d6a179388e3b9ed02e51fadeddd6946
Sashko Stubailo <sashko@stubailo.com>
2015-04-29 10:09:08 -0700

    Bump version

--- packages/json-routes/package.js
+++ packages/json-routes/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'simple:json-routes',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'The simplest way to define server-side routes that return JSON',
   // URL to the Git repository containing the source code for this package.

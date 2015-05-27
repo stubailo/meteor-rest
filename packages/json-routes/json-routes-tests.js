8296be9c50c8b2caec5282330c5a8c932e89200d
Kahmali Rose <krose72205@gmail.com>
2015-05-27 04:05:15 -0400

    Make 'method' param in JsonRoutes.add() case-insensitive
    json-routes:
    - Resolve Issue #31
    - Update README
      - Add info on param being case-insensitive
      - Enforce 80 character line limit
      - Create a table of links for long URLs
      - Fix indentation in bullet-lists to improve readability of raw README
    - Update change log

--- packages/json-routes/json-routes-tests.js
+++ packages/json-routes/json-routes-tests.js
@@ -1,1 +1,34 @@
-// This package is tested in the `simple:rest` package.
+// This package is also tested in the `simple:rest` package.
+if (Meteor.isServer) {
+  JsonRoutes.add("GET", "case-insensitive-method-1", function (req, res) {
+    JsonRoutes.sendResult(res, 200, true);
+  });
+  JsonRoutes.add("Get", "case-insensitive-method-2", function (req, res) {
+    JsonRoutes.sendResult(res, 200, true);
+  });
+  JsonRoutes.add("get", "case-insensitive-method-3", function (req, res) {
+    JsonRoutes.sendResult(res, 200, true);
+  });
+}
+else { // Meteor.isClient
+  testAsyncMulti("JSON Routes - should support case-insensitive HTTP method types", [
+    function (test, expect) {
+      HTTP.get("/case-insensitive-method-1", expect(function (err, res) {
+        test.equal(err, null);
+        test.equal(res.data, true);
+      }));
+    },
+    function (test, expect) {
+      HTTP.get("/case-insensitive-method-2", expect(function (err, res) {
+        test.equal(err, null);
+        test.equal(res.data, true);
+      }));
+    },
+    function (test, expect) {
+      HTTP.get("/case-insensitive-method-3", expect(function (err, res) {
+        test.equal(err, null);
+        test.equal(res.data, true);
+      }));
+    }
+  ]);
+}
